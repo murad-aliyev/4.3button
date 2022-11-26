f6e29009782bebb05a7d55cf9700cb2a41e83344
murad-aliyev <98873214+murad-aliyev@users.noreply.github.com>
2022-11-27 00:20:05 +0400

    Add files via upload

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -2,14 +2,25 @@ import React,{Component} from 'react'
 import './style.css'
 
 class SubscribeButton extends Component {
+
+  state = {
+    text: "Subscribe",
+    subscribed: false,
+    style: "button-state1"
+  }
+
+
   clickHandler = (event) => {
-    console.log('Button click!');
+    this.setState({ text: ([this.state.text]=="Subscribe")?"Subscribed":"Subscribe", 
+    subscribed: [!this.state.subscribed],
+    style: ([this.state.style]=="button-state1")?"button-state2":"button-state1"
+  });
   };
  
   render() {
     return (
-      <div><button type="button" onClick={this.clickHandler}>
-      Subscribe
+      <div><button className={this.state.style} type="button" onClick={this.clickHandler}>
+      {this.state.text}
    </button></div>
     );
   }
